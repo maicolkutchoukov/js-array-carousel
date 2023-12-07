53f8808f17255feb13467a0cc8f7cef03cc7e087
maicolkutchoukov <maicol.j.95@gmail.com>
2023-12-07 16:22:30 +0100

    Completato esercizio

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# template-html-css
+Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
+Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+MILESTONE 3
+Al click dell'utente sulle frecce, il programma cambierà l'immagine attiva, che quindi verrà visualizzata al posto della precedente.
+Prima di partire a scrivere codice:
+Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima, come abbiamo fatto sempre, cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche giorno la sessione HTML e CSS, se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista il focus dell'esercizio.
+Consigli del giorno:
+1. Costruiamo del carosello una versione statica contenente solamente un'immagine. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template".
+2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
+3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"
+Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,82 @@
-console.log('Template html-css-js');
+/*
+
+MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e 
+inseriamo tutte le immagini dinamicamente
+servendoci dell'array fornito e un semplice 
+ciclo for che concatena un template literal.
+Tutte le immagini saranno nascoste, tranne la prima, 
+che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo 
+con lo stesso slider stilato nella milestone 1, 
+ma costruito dinamicamente attraverso JavaScript.
+
+*/
+
+const img1 = document.createElement('img');
+img1.src = 'img/01.webp';
+img1.classList = 'active'
+const img2 = document.createElement('img');
+img2.src = 'img/02.webp';
+const img3 = document.createElement('img');
+img3.src = 'img/03.webp';
+const img4 = document.createElement('img');
+img4.src = 'img/04.webp';
+const img5 = document.createElement('img');
+img5.src = 'img/05.webp';
+
+const imgArray = [img1, img2, img3, img4, img5];
+const carousel = document.querySelector('.carousel-container');
+let itemsImg = document.querySelector('.items')
+
+
+
+for (let i = 0; i < imgArray.length; i++) {    
+    itemsImg.append(imgArray[i]);
+}
+
+/*
+
+MILESTONE 3
+Al click dell'utente sulle frecce, il programma cambierà l'immagine attiva, 
+che quindi verrà visualizzata al posto della precedente.
+Prima di partire a scrivere codice:
+Non lasciamoci spaventare dalla complessità apparente dell'esercizio, 
+ma analizziamo prima, come abbiamo fatto sempre, cosa ci potrebbe aspettare.
+Abbiamo completato ormai da qualche giorno la sessione HTML e CSS, 
+se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. 
+Non dedichiamo però al ripasso più di una mezz'ora, 
+così da non perdere di vista il focus dell'esercizio.
+
+*/
+
+const buttonPrev = document.querySelector('.prev');
+const buttonNext = document.querySelector('.next');
+let contatore = 0
+
+// Creazione bottone scorrimento 'Foto successiva'
+
+buttonNext.addEventListener('click', function(){
+    
+    if (contatore <= 3){
+        imgArray[contatore].classList.remove('active');
+        console.log(imgArray[contatore]);
+        contatore++;
+        imgArray[contatore].classList.add('active');       
+        console.log(imgArray[contatore]);
+    }    
+})
+
+// Creazione bottone scorrimento 'Foto Precedente'
+
+buttonPrev.addEventListener('click', function(){
+    
+    if (contatore > 0){
+        imgArray[contatore].classList.remove('active');
+        console.log(imgArray[contatore]);
+        contatore--;
+        imgArray[contatore].classList.add('active');       
+        console.log(imgArray[contatore]);
+    }    
+})
+
